a890550b46a9026c434f6ee1f6e75b56d3236f01
Juan Rodriguez Hortala <juan.rodriguez.hortala@gmail.com>
2015-07-16 10:39:25 +0200

    added travis minimal build for ScalaCheck #393

---
+++ src/test/scala/es/ucm/fdi/sscheck/testing/SequentialScalacheckOriginal.scala
@@ -0,0 +1,71 @@
+package es.ucm.fdi.sscheck.testing
+
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import org.specs2.ScalaCheck
+import org.specs2.scalacheck.{Parameters, ScalaCheckProperty}
+import org.scalacheck.{Prop, Gen}
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Prop.AnyOperators
+
+import org.specs2.specification.BeforeAfterEach
+
+@RunWith(classOf[JUnitRunner])
+class SequentialScalacheckOriginal extends org.specs2.Specification 
+                     with org.specs2.matcher.MustThrownExpectations
+                     with BeforeAfterEach
+                     with ScalaCheck {
+  def is = 
+   sequential ^
+   "Example run should be sequential with ScalaCheck configured for one worker" ^
+     "property 1"  ! prop1 ^ 
+     "property 2"  ! prop2 ^
+     end
+
+  var counters = scala.collection.mutable.Map("example" -> 0, "test case" -> 0, "prop1" -> 0)
+
+  def enter(counterId : String) : Unit = this.synchronized {
+    println(s"one more $counterId")
+    counters(counterId) = counters(counterId) + 1
+    if (counters(counterId) > 1) {
+      println(s"too many $counterId")
+      throw new RuntimeException("this should be sequential")
+    }
+  }
+
+  def exit(counterId : String) : Unit = this.synchronized { 
+    println(s"one less $counterId")
+    counters(counterId) -= 1
+  }
+
+  var once = false
+  def enterOnce() : Unit = this.synchronized {
+    if (once) {
+      throw new RuntimeException("this should be executed just once")
+    }
+    once = true
+  }
+
+  override def before : Unit = enter("example")
+  override def after : Unit = exit("example")
+
+  def prop1 = {
+    enter("prop1") 
+    enterOnce()
+    val p = 
+      Prop.forAll ("x" |: Gen.choose(0, 100)) { x : Int => 
+        print(s"$x,")
+        enter("test case")
+        x must be_>=(0)
+        exit("test case")
+        true
+    } . set(workers = 1, minTestsOk = 150).verbose // comment the set and this works ok
+   exit("prop1")
+   p
+  }
+
+  def prop2 = {
+    println("running prop2")
+    2 === 2
+  }
+}
