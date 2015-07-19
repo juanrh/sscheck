a791d9b4cab4b0f6787aac5a522dbb2dce74a26d
Juan Rodriguez Hortala <juan.rodriguez.hortala@gmail.com>
2015-07-19 18:19:19 +0200

    inline alternative implementation based on SyncVar

--- src/main/scala/es/ucm/fdi/sscheck/spark/streaming/StreamingContextUtils.scala
+++ src/main/scala/es/ucm/fdi/sscheck/spark/streaming/StreamingContextUtils.scala
@@ -5,7 +5,7 @@ import scala.concurrent.duration._
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.scheduler.{StreamingListener, StreamingListenerReceiverStarted, StreamingListenerBatchCompleted}
 
-
+// Inline alternative implementation based on SyncVar
 object StreamingContextUtils {
   /** This is a blocking call that awaits (with scala.concurrent.Await.result) for the receiver 
    *  of the input Streaming Context to complete start. This can be used to avoid sending data 
@@ -14,11 +14,14 @@ object StreamingContextUtils {
   def awaitUntilReceiverStarted(ssc : StreamingContext, 
                                 atMost : scala.concurrent.duration.Duration = 2 seconds) : Unit = {
     val receiverStartedPromise = Promise[Unit]
+    // val sv = new SyncVar[Unit]
     ssc.addStreamingListener(new StreamingListener {
       override def onReceiverStarted(receiverStarted: StreamingListenerReceiverStarted) : Unit = {
-        receiverStartedPromise success ()  
+        receiverStartedPromise success ()
+        // sv.put(())
       }
     })
+    // sv.get(atMost toMillis)
     Await.result(receiverStartedPromise.future, atMost)
   }
 }
