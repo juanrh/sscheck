b18347613487a2e3b2479e2ad011ae8f88725838
Juan Rodriguez Hortala <juan.rodriguez.hortala@gmail.com>
2015-08-25 19:29:30 +0200

    implement first prop for streaming testing: forall always #15

---
+++ src/main/scala/es/ucm/fdi/sscheck/prop/DStreamProp.scala
@@ -0,0 +1,134 @@
+package es.ucm.fdi.sscheck.prop
+
+import org.scalacheck.{Properties, Gen}
+import org.scalacheck.Prop.{forAll, exists, AnyOperators}
+import org.scalacheck.Prop
+import org.scalacheck.util.Pretty
+
+import org.specs2.matcher.MatchFailureException
+import org.specs2.execute.{AsResult,Result}
+import org.specs2.execute.StandardResults
+import org.specs2.scalacheck.AsResultProp
+
+import org.apache.spark._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.scheduler.{StreamingListener, StreamingListenerBatchCompleted}
+import scala.reflect.ClassTag
+import scala.concurrent.SyncVar
+
+import com.typesafe.scalalogging.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import scala.util.Properties.lineSeparator
+
+import org.apache.spark.streaming.dstream.DynSeqQueueInputDStream
+import es.ucm.fdi.sscheck.spark.Parallelism
+import es.ucm.fdi.sscheck.{TestCaseIdCounter,PropResult,TestCaseId,TestCaseRecord}
+  
+object DStreamProp {
+  @transient private val logger = Logger(LoggerFactory.getLogger("DStreamProp"))
+  
+  // Constants used for printing a sample of the generated values for each batch
+  val msgHeader = "-"*43
+  val numSampleRecords = 4
+  
+  /** Define a ScalaCheck property for 1 input dstream and 1 transformed dstream, 
+   *  that succeeds iff all the assertions hold for each batch
+   *  
+   * NOTE: currently only supports a single ScalaCheck worker
+   * */
+  def forAllAlways[E1:ClassTag,E2:ClassTag,P]
+    (g1: Gen[Seq[Seq[E1]]])(gt1 : (DStream[E1]) => DStream[E2])(assertions: (RDD[E1], RDD[E2]) => P)
+    (implicit pv: P => Prop, rv : P => Result, pp1: Seq[Seq[E1]] => Pretty, 
+                            ssc : StreamingContext, parallelism : Parallelism): Prop = {
+    
+    val inputDStream1 = new DynSeqQueueInputDStream[E1](ssc, numSlices = parallelism.numSlices)
+    inputDStream1.foreachRDD { (rdd, time) => 
+      println(s"""${msgHeader}
+Time: ${time} - InputDStream1 (${rdd.count} records)
+${msgHeader}
+${rdd.take(numSampleRecords).mkString(lineSeparator)}
+...""")
+    }
+    val transformedStream1 = gt1(inputDStream1) 
+    // note we access transformedStream1 but only in slice(), so we need some trivial action
+    // on transformedStream1 or we get org.apache.spark.SparkException: org.apache.spark.streaming.dstream.MappedDStream@459bd6af has not been initialized)
+    transformedStream1.foreachRDD {rdd => {}}
+    
+    var propResult : Option[Result] = None     
+    
+    inputDStream1.foreachRDD { (input1Batch, time) =>
+      // NOTE: batch cannot be completed until this code finishes, use
+      // future if needed to avoid blocking the batch
+      if ((! propResult.isDefined) && input1Batch.count > 0) {  
+        val thisBatchResult = {
+          val trans1Batch = transformedStream1.slice(time, time).head
+          AsResult { assertions(input1Batch, trans1Batch) }
+        }
+        if (thisBatchResult.isFailure || thisBatchResult.isError || thisBatchResult.isThrownFailure) {
+          // Checking according to https://etorreborre.github.io/specs2/guide/SPECS2-3.6.2/org.specs2.guide.StandardResults.html 
+          // Invariant: propResult is assigned to Some at most once, only for the first counterexample 
+          // found. This way we ensure we don't mix errors from different test cases 
+          propResult = Some(thisBatchResult)
+        }
+      }
+    }
+    
+    ssc.start()
+    
+    // Synchronization stuff
+    // test case id counter / generator
+    val testCaseIdCounter = new TestCaseIdCounter
+    // won't wait for each batch for more than batchCompletionTimeout milliseconds
+    val batchCompletionTimeout = inputDStream1.slideDuration.milliseconds * 10
+    // the worker thread uses the SyncVar with a registered addStreamingListener
+    // that notifies onBatchCompleted.
+    // Note: no need to wait for receiver, as there is no receiver
+    val onBatchCompletedSyncVar = new SyncVar[Unit]
+    ssc.addStreamingListener(new StreamingListener {
+      override def onBatchCompleted(batchCompleted: StreamingListenerBatchCompleted) : Unit =  {
+        // signal the property about the completion of a new batch  
+        if (! onBatchCompletedSyncVar.isSet) {
+          // note only this threads makes puts, so no problem with concurrency
+          onBatchCompletedSyncVar.put(())
+        }
+      }
+    })
+    
+    Prop.forAllNoShrink (g1) { (testCaseDstream : Seq[Seq[E1]]) =>
+      val testCaseId : TestCaseId = testCaseIdCounter.nextId() 
+      var testCaseResult : Result = StandardResults.success
+      // to stop in the middle of the test case as soon as a counterexample is found 
+      // Note: propFailed is not equivalent to propResult.isDefined, because propFailed is
+      // only defined after a wait for onBatchCompleted
+      var propFailed = false 
+      logger.warn(s"starting test case $testCaseId")
+      
+      // add current test case to inputDStream
+      // val inputDStream1 = new DynSeqQueueInputDStream[E1](ssc, numSlices = parallelism.numSlices)
+      inputDStream1.addDStream(testCaseDstream)
+      
+      for (i <- 1 to testCaseDstream.length if (! propFailed)) {
+        // await for the end of the each batch 
+        logger.debug(s"waiting end of batch ${i} of test case ${testCaseId} at thread ${Thread.currentThread()}")
+        onBatchCompletedSyncVar.take(batchCompletionTimeout) // use the SyncVar to wait for batch completion  
+        logger.debug(s"awake after end of batch ${i} of test case ${testCaseId} at thread ${Thread.currentThread()}")        
+        if (propResult.isDefined) {
+          // some batch generated a counterexample
+          propFailed = true  
+          testCaseResult = propResult.get
+        }
+        // else do nothing, as the data is already sent 
+      }
+        
+      // Note: ScalaCheck will show the correct test case that caused the counterexample
+      // because only the test case generated that counterexample will fail. Anyway we could
+      // use testCaseResult.mapMessage here to add testCaseDstream to the message of 
+      // testCaseResult if we needed it
+      logger.warn(s"finished test case $testCaseId with result $testCaseResult")
+      AsResultProp.asResultToProp(testCaseResult)
+    } 
+  }
+}
---
+++ src/test/scala/es/ucm/fdi/sscheck/spark/streaming/ScalaCheckStreamingTest.scala
@@ -0,0 +1,69 @@
+package es.ucm.fdi.sscheck.spark.streaming
+
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import org.specs2.ScalaCheck
+import org.specs2.execute.{AsResult, Result}
+
+import org.scalacheck.{Prop, Gen}
+import org.scalacheck.Arbitrary.arbitrary
+
+import org.apache.spark._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.{Duration}
+import org.apache.spark.streaming.dstream.DStream
+
+import es.ucm.fdi.sscheck.prop.DStreamProp
+import es.ucm.fdi.sscheck.matcher.specs2.RDDMatchers._
+
+@RunWith(classOf[JUnitRunner])
+class ScalaCheckStreamingTest 
+  extends org.specs2.Specification  
+  // with org.specs2.matcher.MustThrownExpectations
+  with SharedStreamingContextBeforeAfterEach
+  with org.specs2.matcher.ResultMatchers
+  with ScalaCheck {
+    
+  override def sparkMaster : String = "local[*]"
+  override def batchDuration = Duration(100)
+  
+  val batchSize = 30  
+  val zeroSeqSeq = Gen.listOfN(10,  Gen.listOfN(batchSize, 0)) 
+  val oneSeqSeq = Gen.listOfN(10, Gen.listOfN(batchSize, 1)) 
+  val dsgenSeqSeq1 = Gen.oneOf(zeroSeqSeq, oneSeqSeq)  
+  
+  def is = 
+    sequential ^ s2"""
+    Simple properties for Spark Streaming
+      - where the first property is a success $prop1
+      - where a simple property for DStream.count is a success ${meanProp(_.count)}
+      - where a faulty implementation of the DStream.count is detected ${meanProp(faultyCount) must beFailing}
+    """    
+      
+  def prop1 = 
+    DStreamProp.forAllAlways(
+      "inputDStream" |: dsgenSeqSeq1)(
+      (inputDs : DStream[Int]) => {  
+        val transformedDs = inputDs.map(_+1)
+        transformedDs
+      })( 
+      (inputBatch : RDD[Int], transBatch : RDD[Int]) => {
+        inputBatch.count === transBatch.count and
+        (inputBatch.intersection(transBatch).isEmpty should beTrue) and
+        (  inputBatch should foreachRecord(_ == 0) or 
+           (inputBatch should foreachRecord(_ == 1)) 
+        )
+      }).set(workers = 1, minTestsOk = 10).verbose      
+
+  def faultyCount(ds : DStream[Double]) : DStream[Long] = 
+    ds.count.transform(_.map(_ - 1))
+    
+  def meanProp(testSubject : DStream[Double] => DStream[Long]) = 
+    DStreamProp.forAllAlways(
+      Gen.listOfN(10,  Gen.listOfN(30, arbitrary[Double])))(
+      testSubject)( 
+      (inputBatch : RDD[Double], transBatch : RDD[Long]) => {
+        transBatch.count === 1 and
+        inputBatch.count === transBatch.first
+      }).set(workers = 1, minTestsOk = 10).verbose         
+}
